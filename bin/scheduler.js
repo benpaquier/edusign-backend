fd1a1db2cdf101699520efba161d36fb228784fb
benpaquier <hello@benpaquier.com>
2022-01-13 12:23:20 +0100

    Test

--- bin/scheduler.js
+++ bin/scheduler.js
@@ -4,11 +4,18 @@ const job = () => {
   const number = Math.floor(Math.random() * 1000) + 1
   const array = [number]
   fs.readFile('./data.json', (err, data) => {
-    console.log(err)
+    console.log("first err", err)
+    console.log("first read")
     console.log(JSON.parse(data))
 
     fs.writeFile('./data.json', JSON.stringify(array), (err) => {
-      console.log(err)
+      console.log("write error", err)
+
+      fs.readFile('./data.json', (err, data) => {
+        console.log("second read error", err)
+        console.log("second read")
+        console.log(JSON.parse(data))
+      })
     })
   })
 
